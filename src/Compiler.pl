d8b3676d859320a10be3b8efd6c62af9a29ee378
Prmudire <81540951+Prmudire@users.noreply.github.com>
2021-04-18 17:04:36 -0700

    Update Compiler.pl
    Adding compiler code for "For loop", "For range loop", basic boolean evaluations, declare variables, assign values, display variable value.

--- src/Compiler.pl
+++ src/Compiler.pl
@@ -1,3 +1,53 @@
+% Evaluation expressions ----------------------------------------------
+
+program_eval(start(T)) :-eval_block(T,_,_).
+
+eval_block(block(T),Env,NewEnv) :- eval_command_list(T,Env,NewEnv),!.
+
+% Evaluations of various commands.
+eval_command_list(cmd(T1,T2),Env,NewEnv) :- eval_command(T1,Env,Env1),eval_command_list(T2,Env1,NewEnv).
+eval_command_list(T,Env,NewEnv) :- eval_command(T,Env,NewEnv).
+eval_command(declare(T1,T2),Env,NewEnv) :- eval_id(T2,Id), atom(Id),defaultValue(T1,Val),insert(Id,T1,Val,Env,NewEnv).
+eval_command(assign(T1,T2),Env,NewEnv):- eval_expr(T2,Env,Env1,Res1),eval_id(T1,Id), update(Id,Res1,Env1,NewEnv).
+eval_command(for(T1,T2),Env,NewEnv) :- eval_loopscope_initialize(T1,Env,Env1,true),eval_block(T2,Env1,Env2),eval_command(forLoop(T1,T2),Env2,NewEnv).
+eval_command(for(T1,_),Env,NewEnv) :- eval_loopscope_initialize(T1,Env,NewEnv,false).
+eval_command(forLoop(T1,T2),Env,NewEnv) :- eval_loopscope(T1,Env,Env1,true),eval_block(T2,Env1,Env2),eval_command(forLoop(T1,T2),Env2,NewEnv).
+eval_command(forLoop(T1,_),Env,NewEnv) :- eval_loopscope(T1,Env,NewEnv,false).
+eval_command(display(T),Env,Env):- eval_id(T,Id),lookup(Id,Env,Res),writeln(Res).
+eval_command(block(T),Env,NewEnv):- eval_block(block(T),Env,NewEnv).
+
+eval_command(forRange(T1,T2,T3,T4),Env,NewEnv):-eval_value(T2,InitialVal),eval_id(T1,Id),update(Id,InitialVal,Env,Env1),eval_bool(lessThan(T1,T3),Env1,Env2,true),
+    eval_block(T4,Env2,Env3),
+    lookup(Id,Env3,Val),
+    NewVal is Val+1,    
+	update(Id,NewVal,Env3,Env4),
+    eval_command(forRangeLoop(T1,T3,T4),Env4,NewEnv).
+eval_command(forRange(T1,T2,T3,_),Env,NewEnv):- eval_value(T2,Val),eval_id(T1,Id),update(Id,Val,Env,Env1),eval_bool(lessThan(T1,T3),Env1,NewEnv,false).
+eval_command(forRangeLoop(T1,T3,T4),Env,NewEnv):-eval_id(T1,Id), eval_bool(lessThan(T1,T3),Env,Env1,true),
+    eval_block(T4,Env1,Env2),
+    lookup(Id,Env2,Val),    
+    NewVal is Val+1,    
+	update(Id,NewVal,Env2,Env3),
+    eval_command(forRangeLoop(T1,T3,T4),Env3,NewEnv).
+eval_command(forRangeLoop(T1,T3,_),Env,NewEnv):- eval_bool(lessThan(T1,T3),Env,NewEnv,false).
+
+defaultValue('int',0).
+defaultValue('bool',false).
+defaultValue('string','').
+
+% Boolean expressions evaluation.
+eval_comp_bool(T,Env,NewEnv,Res):- eval_bool(T,Env,NewEnv,Res).
+eval_bool(isEqual(T1,T2),Env,NewEnv,Val) :- eval_expr(T1,Env,Env1,Res1),eval_expr(T2,Env1,NewEnv,Res2), eval_equality(Res1,Res2,Val).
+eval_bool(lessThan(T1,T2),Env,NewEnv,Val) :- eval_expr(T1,Env,Env1,Res1),eval_expr(T2,Env1,NewEnv,Res2), eval_lessThan(Res1,Res2,Val).
+eval_bool(greaterThan(T1,T2),Env,NewEnv,Val) :- eval_expr(T1,Env,Env1,Res1),eval_expr(T2,Env1,NewEnv,Res2), eval_greaterThan(Res1,Res2,Val).
+eval_bool(true,Env,Env,true).
+eval_bool(false,Env,Env,false).
+
+eval_loopscope_initialize(loopScope(T1,T2,_),Env,NewEnv,true):- eval_command(T1,Env,Env1),eval_comp_bool(T2,Env1,NewEnv,true).
+eval_loopscope_initialize(loopScope(T1,T2,_),Env,NewEnv,false):- eval_command(T1,Env,Env1),eval_comp_bool(T2,Env1,NewEnv,false).
+eval_loopscope(loopScope(_,T2,T3),Env,NewEnv,true):- eval_command(T3,Env,Env1),eval_comp_bool(T2,Env1,NewEnv,true).
+eval_loopscope(loopScope(_,T2,T3),Env,NewEnv,false):- eval_command(T3,Env,Env1),eval_comp_bool(T2,Env1,NewEnv,false).
+
 eval_value(t_integer(X),X):- number(X).
 eval_value(t_string(X),X):- atom(X). % NEED TO CHECK FURTHER
 eval_value(t_boolean(X),X).
