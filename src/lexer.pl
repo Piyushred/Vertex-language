923fc26f3e769fb63970eb49bb6e89183e4385a8
riteshreddy18 <71104856+riteshreddy18@users.noreply.github.com>
2021-04-21 23:42:01 -0700

    Lexer update

--- src/lexer.pl
+++ src/lexer.pl
@@ -1,3 +1,11 @@
+%read the input stream
+input(InStream,[]):-at_end_of_stream(InStream).
+input(InStream,[TokenCode|RemTokens]):-input(InStream,RemTokens), get_code(InStream,TokenCode).
+
+% Checks for terminators/delimitiers
+wordSplit([Code1,Code2|Rem],[Code1|Words],Res):-char_type(Code2,alnum), wordSplit([Code2|Rem],Words,Res).
+wordSplit([Code1|Rem],[Code1],Rem).
+
 % Generates each token
 % atom_string changes the right parameter into the string equivalent
 % name generates the character code
@@ -6,3 +14,5 @@ token([],[]).
 token([Code|Rem],Tokens):-char_type(Code,space),token(Rem,Tokens),!.
 token([Code|Codes],[Strings|Tokens]):-char_type(Code,alnum), wordSplit([Code|Codes],Words,Rem), name(Word,Words), atom_string(Word,Strings), token(Rem,Tokens),!.
 token([Code|Rem],[Strings|Tokens]):-name(Char,[Code]), atom_string(Char,Strings), token(Rem,Tokens).
+
+
